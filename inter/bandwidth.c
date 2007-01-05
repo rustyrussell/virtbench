6e599b074a657a45eefaa25085669b672ab86d4f
rusty@debussy.beldaran.org <none@none>
2007-01-05 16:39:49 +1100

    New inter-domain bandwidth test, and don't keep 64MB alloc around in memburn (if every test did that...)

---
+++ inter/bandwidth.c
@@ -0,0 +1,99 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <err.h>
+#include "../benchmarks.h"
+
+#define BANDWIDTH_SIZE 4 MB
+static const char fmtstr[] 
+= "Time to send " __stringify(BANDWIDTH_SIZE) " between guests: %u nsec";
+#define MB * 1024 * 1024
+
+#define HIPQUAD(ip)				\
+	((u8)(ip >> 24)),			\
+	((u8)(ip >> 16)),			\
+	((u8)(ip >> 8)),			\
+	((u8)(ip))
+
+static void do_bandwidth_bench(int fd, u32 runs,
+			       struct benchmark *bench, const void *opts)
+{
+	/* We're going to send TCP packets to that addr. */
+	struct sockaddr_in saddr;
+	int sock, ret, done = 0;
+	const struct pair_opt *opt = opts;
+	char *mem = malloc(BANDWIDTH_SIZE);
+
+	if (!mem)
+		err(1, "allocating %i bytes", BANDWIDTH_SIZE);
+
+	sock = socket(PF_INET, SOCK_STREAM, 0);
+	if (sock < 0)
+		err(1, "creating socket");
+
+	if (opt->start) {
+		/* We accept connection from other client. */
+		int listen_sock = sock;
+		int set = 1;
+
+		saddr.sin_family = AF_INET;
+		saddr.sin_port = htons(6100);
+		saddr.sin_addr.s_addr = htonl(opt->yourip);
+		if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &set, sizeof(set)) != 0)
+			warn("setting SO_REUSEADDR");
+		if (bind(sock, (struct sockaddr *)&saddr, sizeof(saddr)) != 0)
+			err(1, "binding socket");
+
+		if (listen(sock, 0) != 0)
+			err(1, "listening on socket");
+
+		send_ack(fd);
+
+		sock = accept(listen_sock, NULL, 0);
+		if (sock < 0)
+			err(1, "accepting peer connection on socket");
+		close(listen_sock);
+	} else {
+		/* We connect to other client. */
+		saddr.sin_family = AF_INET;
+		saddr.sin_port = htons(6100);
+		saddr.sin_addr.s_addr = htonl(opt->otherip);
+		if (connect(sock, (struct sockaddr *)&saddr, sizeof(saddr)))
+			err(1, "connecting socket");
+
+		send_ack(fd);
+	}
+
+	if (wait_for_start(fd)) {
+		u32 i;
+		for (i = 0; i < runs; i++) {
+			if (opt->start) {
+				while ((ret = write(sock, mem+done,
+						    BANDWIDTH_SIZE-done)) > 0){
+					done += ret;
+					if (done == BANDWIDTH_SIZE)
+						break;
+				}
+				if (ret <= 0)
+					err(1, "writing to other end");
+			} else {
+				while ((ret = read(sock, mem+done,
+						   BANDWIDTH_SIZE-done)) > 0) {
+					done += ret;
+					if (done == BANDWIDTH_SIZE)
+						break;
+				}
+				if (ret <= 0)
+					err(1, "reading from other end");
+			}
+		}
+		send_ack(fd);
+	}
+	close(sock);
+	free(mem);
+}
+
+static struct benchmark bandwidth_benchmark _benchmark_
+= { "bandwidth", fmtstr, do_pair_bench, do_bandwidth_bench };
+
--- micro/memburn.c
+++ micro/memburn.c
@@ -13,31 +13,25 @@ static const char fmtstr_random[]
 = "Time to walk random " __stringify(MEMBURN_SIZE) ": %u nsec";
 #define MB * 1024 * 1024
 
-static char *mem = NULL;
-static unsigned int pagesize;
-
-static void setup(void)
+static void *setup(void)
 {
 	unsigned int i;
+	char *mem;
 
-	/* Alloc once, so we don't thrash. */
-	if (!mem) {
-		mem = malloc(MEMBURN_SIZE);
-		if (!mem)
-			err(1, "allocating %i bytes", MEMBURN_SIZE);
-		pagesize = getpagesize();
-	}
+	mem = malloc(MEMBURN_SIZE);
+	if (!mem)
+		err(1, "allocating %i bytes", MEMBURN_SIZE);
 
-	for (i = 0; i < MEMBURN_SIZE; i+= pagesize)
+	for (i = 0; i < MEMBURN_SIZE; i+= getpagesize())
 		mem[i] = i;
+	return mem;
 }
 
 static void do_memburn_linear(int fd, u32 runs,
 			      struct benchmark *bench, const void *opts)
 {
-	unsigned int r, i;
-
-	setup();
+	unsigned int r, i, pagesize = getpagesize();
+	char *mem = setup();
 
 	send_ack(fd);
 	if (wait_for_start(fd)) {
@@ -46,15 +40,15 @@ static void do_memburn_linear(int fd, u32 runs,
 				mem[i] = i;
 		send_ack(fd);
 	}
+	free(mem);
 }
 
 static void do_memburn_random(int fd, u32 runs,
 			      struct benchmark *bench, const void *opts)
 {
-	unsigned int r, i;
-	unsigned int random_table[MEMBURN_SIZE / getpagesize()];
-
-	setup();
+	unsigned int r, i, pagesize = getpagesize();;
+	unsigned int random_table[MEMBURN_SIZE / pagesize];
+	char *mem = setup();
 
 	for (i = 0; i < ARRAY_SIZE(random_table); i++)
 		random_table[i] = i * pagesize;
@@ -73,6 +67,7 @@ static void do_memburn_random(int fd, u32 runs,
 				mem[random_table[i]] = i;
 		send_ack(fd);
 	}
+	free(mem);
 }
 
 struct benchmark memburn_linear _benchmark_
